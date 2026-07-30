eb7229099f3a62574298c6788a04936f7ce3c879
AdityaSingh <mayankaditya35@gmail.com>
2026-07-30 20:43:33 +0530

    Certification update page created

--- src/data/about/certificatesData.js
+++ src/data/about/certificatesData.js
@@ -2,44 +2,40 @@ const certificates = [
   {
     id: 1,
     title: "Javascript Certificate",
-    organization: "Scaler",
+    organization: "Hackerrank",
     image:
-      "/images/certificates/image_0f72decb-e842-4619-972b-05169165f36520241227_211115.webp",
+      "/images/certificates/javascript_intermediate certificate_page-0001.jpg",
   },
   {
     id: 2,
-    title: "Bootstrap Certificate",
-    organization: "Udemy",
+    title: "Frontend Developer(React) Certificate",
+    organization: "Hackerrank",
     image:
-      "/images/certificates/image_6f8ee3f6-7be5-4412-a728-a4f199a6864e20241227_211125.webp",
+      "/images/certificates/frontend_developer_react certificate_page-0001.jpg",
   },
   {
     id: 3,
-    title: "MERN Development Certificate",
-    organization: "Infosys Springboard",
-    image:
-      "/images/certificates/image_8024334f-17e1-48b0-9cf2-31cf1279f94320241227_211129.webp",
+    title: "SQL Certificate",
+    organization: "Hackerrank",
+    image: "/images/certificates/sql.jpg",
   },
   {
     id: 4,
-    title: "React JS Certificate",
-    organization: "Infosys Springboard",
-    image:
-      "/images/certificates/image_b4994c1a-6134-4caa-b40b-41920bea0d6120241227_211133.webp",
+    title: "React Fundamentals Certificate",
+    organization: "Iskima",
+    image: "/images/certificates/React_Dev.jpg",
   },
   {
     id: 5,
-    title: "MERN Stack Certificate",
-    organization: "Udemy",
-    image:
-      "/images/certificates/image_cdc4b616-2f43-4e8a-8a46-356fc454c58420241227_211137.webp",
+    title: "Prompt Engineering Certificate",
+    organization: "AWS",
+    image: "/images/certificates/Prompt.jpg",
   },
   {
     id: 6,
-    title: "Introduction to MERN Stack",
-    organization: "Simplilearn",
-    image:
-      "/images/certificates/image_e8b14a5e-1297-4c6c-9d3e-da0f24ab509c20241227_211108.webp",
+    title: "Databases for Developers Certificate",
+    organization: "Oracle",
+    image: "/images/certificates/Databases Oracle.jpg",
   },
 ];
 
